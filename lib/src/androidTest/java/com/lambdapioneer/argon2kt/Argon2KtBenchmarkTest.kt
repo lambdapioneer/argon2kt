bb82862d36abf2457e64c41b36c7a55e1cfea6db
Daniel Hugenroth <daniel.hugenroth@gmail.com>
2024-09-06 11:26:13 +0100

    Remove superfluous semicolons

--- lib/src/androidTest/java/com/lambdapioneer/argon2kt/Argon2KtBenchmarkTest.kt
+++ lib/src/androidTest/java/com/lambdapioneer/argon2kt/Argon2KtBenchmarkTest.kt
@@ -5,7 +5,7 @@
 
 package com.lambdapioneer.argon2kt
 
-import com.google.common.truth.Truth.assertThat;
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class Argon2KtBenchmarkUtilsInstrumentedTest {
