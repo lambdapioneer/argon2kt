751494a93d1b870a24324ae51efeaa4f377854fc
Daniel Hugenroth <daniel.hugenroth@gmail.com>
2026-09-01 19:27:27 +0100

    Update dependencies; drop multidex

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+buildscript {
+    dependencies {
+        // Use a newer Kotlin compiler than the one bundled with AGP's built-in Kotlin support
+        classpath(libs.kotlin.gradle.plugin)
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
 }
